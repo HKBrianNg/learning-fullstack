8ce047b45dc01095ff279b492964c066bf2c0998
Brian Ng <ngshunchiang@hotmail.com>
2022-08-11 18:05:19 +0800

    add create video UI

---
+++ frontend/src/api/video.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const url = 'http://localhost:4000'
+
+export const createVideoAPI = async (user) => {
+    try {
+        const response = await axios.post(`${url}/user/signup`, user)
+        // console.log("signupAPI() success:", response)
+        return { okStatus: true, data: response.data }
+    } catch (error) {
+        // console.log("signupAPI() error:", error.response.data.error)
+        return { okStatus: false, data: error.response.data.error }
+    }
+}
+
+// export const loginAPI = async (user) => {
+//     try {
+//         const response = await axios.post(`${url}/user/login`, user)
+//         // console.log("loginAPI() success:", response)
+//         return { okStatus: true, data: response.data }
+//     } catch (error) {
+//         // console.log("loginAPI() error:", error.response.data.error)
+//         return { okStatus: false, data: error.response.data.error }
+//     }
+// }
+
+// export const getusersAPI = async (user) => {
+//     try {
+//         const response = await axios.get(`${url}/user`)
+//         console.log("getusersAPI() success:", response)
+//         return { okStatus: true, data: response.data }
+//     } catch (error) {
+//         console.log("getusersAPI() error:", error.response.data.error)
+//         return { okStatus: false, data: error.response.data.error }
+//     }
+// }
