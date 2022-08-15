9b43acf22ed703362b647059ecf4d9c657788221
Brian Ng <ngshunchiang@hotmail.com>
2022-08-15 08:37:52 +0800

    add theme in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,48 @@
 import { useState, createContext } from 'react'
 import { Routes, Route } from "react-router-dom"
-// import Home from './component/home/Home'
 import Login from './page/auth/Login'
 import Signup from './page/auth/Signup'
 import Logout from './page/auth/Logout'
 import VideoDetails from './page/video/VideoDetails'
-// import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Home from './page/home/Home'
+import Topic from './page/config/Topic'
+import User from './page/config/User'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 export const AppContext = createContext(null)
 export const VideoContext = createContext(null)
 
 function App() {
-  const [app, setApp] = useState({ email: '', token: '', currentTab: 0 })
+  const [app, setApp] = useState({ email: '', token: '', theme: 'light', currentTab: 0 })
   const [videoData, setVideoData] = useState([])
 
-  // const [appTheme, setAppTheme] = useState('light');
 
-  // const theme = createTheme({
-  //   palette: {
-  //     mode: appTheme,
-  //   },
-  // });
-
+  const theme = createTheme({
+    palette: {
+      mode: app.theme,
+    },
+  });
 
   return (
-    // <ThemeProvider theme={theme}>
-    <div className="App">
-      <AppContext.Provider value={{ app, setApp }}>
-        <VideoContext.Provider value={{ videoData, setVideoData }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/video/:id" element={<VideoDetails />} />
-            <Route path="/auth/login" element={<Login />} />
-            <Route path="/auth/signup" element={<Signup />} />
-            <Route path="/auth/logout" element={<Logout />} />
-          </Routes>
-        </VideoContext.Provider>
-      </AppContext.Provider>
-    </div>
-    // </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AppContext.Provider value={{ app, setApp }}>
+          <VideoContext.Provider value={{ videoData, setVideoData }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/video/:id" element={<VideoDetails />} />
+              <Route path="/auth/login" element={<Login />} />
+              <Route path="/auth/signup" element={<Signup />} />
+              <Route path="/auth/logout" element={<Logout />} />
+              <Route path="/config/user" element={<User />} />
+              <Route path="/config/topic" element={<Topic />} />
+            </Routes>
+          </VideoContext.Provider>
+        </AppContext.Provider>
+      </div>
+    </ThemeProvider>
   );
 }
 
